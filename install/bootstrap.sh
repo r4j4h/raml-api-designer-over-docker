210a3c415bd527198697e25ce01a6951b3947c51
Jasmine <jasmine@webpt.com>
2014-06-09 18:32:44 +0000

    Re-adding building of node/npm to script so it should get node now :)

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,14 +2,33 @@
 
 echo "Bootstrap script running."
 
+# Install Git
 apt-get -y install git
 
+# Install Node and NPM
+
+# Install Node and NPM > Download and extract node
+wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
+tar -zxvf node-v0.10.26.tar.gz
+rm -r node-v0.10.26.tar.gz
+mv node-v0.10.26 /root/
+ln -sf /root/node-v0.10.26 /root/node
+
+
+# Install Node and NPM > Build node
+cd /root/node
+./configure
+make
+make install
+cd /
+export PATH=$PATH:/root/node
+
+# Install RAML API Designer
 cd /usr/local
 git clone https://github.com/mulesoft/api-designer.git
 cd ./api-designer
 
 #sudo apt-get install nodejs nodejs-dev npm
-exit
 npm install -g grunt-cli
 npm install -g bower
 npm install -g karma # Optional for running the test suite
@@ -28,4 +47,3 @@ grunt server
 
 
 echo "Ready!"
-
